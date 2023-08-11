5ccfbc8aac3cd0af46a8981cc3d54b60ca62d322
Muhammad Salman <salmannaseem1103@gmail.com>
2023-08-11 20:02:04 +0500

    uam update

--- backend/atom/app/models/uam_models.py
+++ backend/atom/app/models/uam_models.py
@@ -17,32 +17,37 @@ from app.models.atom_models import *
 
 class UAM_Device_Table(db.Model):
     __tablename__ = "uam_device_table"
-    uam_id = db.Column(db.Integer, primary_key=True)
-    atom_id = db.Column(db.Integer, ForeignKey("atom_table.atom_id"))
+    uam_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    atom_id = db.Column(
+        db.Integer,
+        ForeignKey("atom_table.atom_id", ondelete="CASCADE", onupdate="CASCADE"),
+        nullable=False,
+    )
 
-    software_type = db.Column(db.String(50))
-    software_version = db.Column(db.String(50))
-    patch_version = db.Column(db.String(50))
-    status = db.Column(db.String(50))
-    manufacturer = db.Column(db.String(50))
-    hw_eos_date = db.Column(db.Date)
-    hw_eol_date = db.Column(db.Date)
-    sw_eos_date = db.Column(db.Date)
-    sw_eol_date = db.Column(db.Date)
-    rfs_date = db.Column(db.Date)
-    authentication = db.Column(db.String(10))
-    serial_number = db.Column(db.String(50))
-    pn_code = db.Column(db.String(50))
-    subrack_id_number = db.Column(db.String(50))
-    manufacturer_date = db.Column(db.Date)
-    hardware_version = db.Column(db.String(50))
-    max_power = db.Column(db.String(50))
-    site_type = db.Column(db.String(50))
-    source = db.Column(db.String(50))
-    stack = db.Column(db.String(50))
-    contract_number = db.Column(db.String(50))
-    contract_expiry = db.Column(db.Date)
-    uptime = db.Column(db.Date)
+    status = db.Column(db.String(50), nullable=False)
+
+    software_type = db.Column(db.String(50), nullable=True)
+    software_version = db.Column(db.String(50), nullable=True)
+    patch_version = db.Column(db.String(50), nullable=True)
+    manufacturer = db.Column(db.String(50), nullable=True)
+    hw_eos_date = db.Column(db.Date, nullable=True)
+    hw_eol_date = db.Column(db.Date, nullable=True)
+    sw_eos_date = db.Column(db.Date, nullable=True)
+    sw_eol_date = db.Column(db.Date, nullable=True)
+    rfs_date = db.Column(db.Date, nullable=True)
+    authentication = db.Column(db.String(10), nullable=True)
+    serial_number = db.Column(db.String(50), nullable=True)
+    pn_code = db.Column(db.String(50), nullable=True)
+    subrack_id_number = db.Column(db.String(50), nullable=True)
+    manufacturer_date = db.Column(db.Date, nullable=True)
+    hardware_version = db.Column(db.String(50), nullable=True)
+    max_power = db.Column(db.String(50), nullable=True)
+    site_type = db.Column(db.String(50), nullable=True)
+    source = db.Column(db.String(50), nullable=True)
+    stack = db.Column(db.String(50), nullable=True)
+    contract_number = db.Column(db.String(50), nullable=True)
+    contract_expiry = db.Column(db.Date, nullable=True)
+    uptime = db.Column(db.Date, nullable=True)
 
     creation_date = db.Column(db.DateTime, default=datetime.now())
     modification_date = db.Column(
@@ -55,20 +60,25 @@ class UAM_Device_Table(db.Model):
 
 class Board_Table(db.Model):
     __tablename__ = "board_table"
-    board_id = db.Column(db.Integer, primary_key=True)
-    uam_id = db.Column(db.Integer, ForeignKey("uam_device_table.uam_id"))
+    board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    uam_id = db.Column(
+        db.Integer,
+        ForeignKey("uam_device_table.uam_id", ondelete="CASCADE", onupdate="CASCADE"),
+        nullable=False,
+    )
 
-    board_name = db.Column(db.String(250))
-    hardware_version = db.Column(db.String(50))
-    device_slot_id = db.Column(db.String(50))
-    software_version = db.Column(db.String(50))
-    serial_number = db.Column(db.String(50))
-    manufacturer_date = db.Column(db.Date)
-    status = db.Column(db.String(50))
-    eos_date = db.Column(db.Date)
-    eol_date = db.Column(db.Date)
-    rfs_date = db.Column(db.Date)
-    pn_code = db.Column(db.String(50))
+    board_name = db.Column(db.String(250), nullable=False)
+    status = db.Column(db.String(50), nullable=False)
+
+    hardware_version = db.Column(db.String(50), nullable=True)
+    device_slot_id = db.Column(db.String(50), nullable=True)
+    software_version = db.Column(db.String(50), nullable=True)
+    serial_number = db.Column(db.String(50), nullable=True)
+    manufacturer_date = db.Column(db.Date, nullable=True)
+    eos_date = db.Column(db.Date, nullable=True)
+    eol_date = db.Column(db.Date, nullable=True)
+    rfs_date = db.Column(db.Date, nullable=True)
+    pn_code = db.Column(db.String(50), nullable=True)
 
     creation_date = db.Column(db.DateTime, default=datetime.now())
     modification_date = db.Column(
@@ -81,23 +91,28 @@ class Board_Table(db.Model):
 
 class Subboard_Table(db.Model):
     __tablename__ = "subboard_table"
-    subboard_id = db.Column(db.Integer, primary_key=True)
-    uam_id = db.Column(db.Integer, ForeignKey("uam_device_table.uam_id"))
+    subboard_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    uam_id = db.Column(
+        db.Integer,
+        ForeignKey("uam_device_table.uam_id", ondelete="CASCADE", onupdate="CASCADE"),
+        nullable=False,
+    )
 
-    subboard_name = db.Column(db.String(250))
-    subboard_type = db.Column(db.String(150))
-    subrack_id = db.Column(db.String(250))
-    slot_number = db.Column(db.String(250))
-    subslot_number = db.Column(db.String(250))
-    hardware_version = db.Column(db.String(50))
-    software_version = db.Column(db.String(50))
-    serial_number = db.Column(db.String(50))
- 
-    status = db.Column(db.String(50))
-    eos_date = db.Column(db.Date)
-    eol_date = db.Column(db.Date)
-    rfs_date = db.Column(db.Date)
-    pn_code = db.Column(db.String(50))
+    subboard_name = db.Column(db.String(250), nullable=False)
+    status = db.Column(db.String(50), nullable=False)
+
+    subboard_type = db.Column(db.String(150), nullable=True)
+    subrack_id = db.Column(db.String(250), nullable=True)
+    slot_number = db.Column(db.String(250), nullable=True)
+    subslot_number = db.Column(db.String(250), nullable=True)
+    hardware_version = db.Column(db.String(50), nullable=True)
+    software_version = db.Column(db.String(50), nullable=True)
+    serial_number = db.Column(db.String(50), nullable=True)
+
+    eos_date = db.Column(db.Date, nullable=True)
+    eol_date = db.Column(db.Date, nullable=True)
+    rfs_date = db.Column(db.Date, nullable=True)
+    pn_code = db.Column(db.String(50), nullable=True)
 
     creation_date = db.Column(db.DateTime, default=datetime.now())
     modification_date = db.Column(
@@ -110,24 +125,30 @@ class Subboard_Table(db.Model):
 
 class Sfps_Table(db.Model):
     __tablename__ = "sfp_table"
-    sfp_id = db.Column(db.Integer, primary_key=True)
-    uam_id = db.Column(db.Integer, ForeignKey("uam_device_table.uam_id"))
+    sfp_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    uam_id = db.Column(
+        db.Integer,
+        ForeignKey("uam_device_table.uam_id", ondelete="CASCADE", onupdate="CASCADE"),
+        nullable=False,
+    )
 
-    media_type = db.Column(db.String(50))
-    port_name = db.Column(db.String(250))
-    port_type = db.Column(db.String(50))
-    connector = db.Column(db.String(50))
-    mode = db.Column(db.String(50))
-    speed = db.Column(db.String(50))
-    wavelength = db.Column(db.String(50))
-    manufacturer = db.Column(db.String(250))
-    optical_direction_type = db.Column(db.String(50))
-    pn_code = db.Column(db.String(50))
-    status = db.Column(db.String(50))
-    eos_date = db.Column(db.Date)
-    eol_date = db.Column(db.Date)
-    rfs_date = db.Column(db.Date)
-    serial_number = db.Column(db.String(50))
+    serial_number = db.Column(db.String(50), nullable=False)
+    status = db.Column(db.String(50), nullable=False)
+
+    media_type = db.Column(db.String(50), nullable=True)
+    port_name = db.Column(db.String(250), nullable=True)
+    port_type = db.Column(db.String(50), nullable=True)
+    connector = db.Column(db.String(50), nullable=True)
+    mode = db.Column(db.String(50), nullable=True)
+    speed = db.Column(db.String(50), nullable=True)
+    wavelength = db.Column(db.String(50), nullable=True)
+    manufacturer = db.Column(db.String(250), nullable=True)
+    optical_direction_type = db.Column(db.String(50), nullable=True)
+    pn_code = db.Column(db.String(50), nullable=True)
+    status = db.Column(db.String(50), nullable=True)
+    eos_date = db.Column(db.Date, nullable=True)
+    eol_date = db.Column(db.Date, nullable=True)
+    rfs_date = db.Column(db.Date, nullable=True)
 
     creation_date = db.Column(db.DateTime, default=datetime.now())
     modification_date = db.Column(
@@ -140,19 +161,25 @@ class Sfps_Table(db.Model):
 
 class License_Table(db.Model):
     __tablename__ = "license_table"
-    uam_id = db.Column(db.Integer, ForeignKey("uam_device_table.uam_id"))
+    license_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    uam_id = db.Column(
+        db.Integer,
+        ForeignKey("uam_device_table.uam_id", ondelete="CASCADE", onupdate="CASCADE"),
+        nullable=False,
+    )
 
-    license_name = db.Column(db.String(250), primary_key=True)
-    license_description = db.Column(db.String(250))
-    rfs_date = db.Column(db.Date)
-    activation_date = db.Column(db.Date)
-    expiry_date = db.Column(db.Date)
-    grace_period = db.Column(db.String(10))
-    serial_number = db.Column(db.String(50))
-    status = db.Column(db.String(50))
-    capacity = db.Column(db.String(50))
-    usage = db.Column(db.String(50))
-    pn_code = db.Column(db.String(50))
+    license_name = db.Column(db.String(250), nullable=False)
+    status = db.Column(db.String(50), nullable=False)
+
+    license_description = db.Column(db.String(250), nullable=True)
+    rfs_date = db.Column(db.Date, nullable=True)
+    activation_date = db.Column(db.Date, nullable=True)
+    expiry_date = db.Column(db.Date, nullable=True)
+    grace_period = db.Column(db.String(10), nullable=True)
+    serial_number = db.Column(db.String(50), nullable=True)
+    capacity = db.Column(db.String(50), nullable=True)
+    usage = db.Column(db.String(50), nullable=True)
+    pn_code = db.Column(db.String(50), nullable=True)
 
     creation_date = db.Column(db.DateTime, default=datetime.now())
     modification_date = db.Column(
@@ -165,40 +192,46 @@ class License_Table(db.Model):
 
 class SNTC_TABLE(db.Model):
     __tablename__ = "sntc_table"
-    sntc_id = db.Column(db.Integer, primary_key=True)
-    pn_code = db.Column(db.String(50))
-    
-    hw_eos_date = db.Column(db.Date)
-    hw_eol_date = db.Column(db.Date)
-    sw_eos_date = db.Column(db.Date)
-    sw_eol_date = db.Column(db.Date)
-    manufacturer_date = db.Column(db.Date)
-    
+    sntc_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    pn_code = db.Column(db.String(50), nullable=False)
+
+    hw_eos_date = db.Column(db.Date, nullable=True)
+    hw_eol_date = db.Column(db.Date, nullable=True)
+    sw_eos_date = db.Column(db.Date, nullable=True)
+    sw_eol_date = db.Column(db.Date, nullable=True)
+    manufacturer_date = db.Column(db.Date, nullable=True)
+
     creation_date = db.Column(db.DateTime, default=datetime.now())
     modification_date = db.Column(
         db.DateTime, default=datetime.now(), onupdate=datetime.now()
     )
-    
+
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 
-
-
 class APS_TABLE(db.Model):
-    __tablename__ = 'aps_table'
-    ap_id = db.Column(db.Integer, primary_key=True)
-    controller_name = db.Column(db.String(50))
-    ap_ip = db.Column(db.String(50))
-    ap_name = db.Column(db.String(50))
-    serial_number = db.Column(db.String(50))
-    ap_model = db.Column(db.String(50))
-    hardware_version = db.Column(db.String(50))
-    software_version = db.Column(db.String(50))
-    description = db.Column(db.String(200))
+    __tablename__ = "aps_table"
+    ap_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    uam_id = db.Column(
+        db.Integer,
+        ForeignKey("uam_device_table.uam_id", ondelete="CASCADE", onupdate="CASCADE"),
+        nullable=False,
+    )
+
+    ap_ip = db.Column(db.String(50), nullable=False)
+
+    ap_name = db.Column(db.String(50), nullable=True)
+    serial_number = db.Column(db.String(50), nullable=True)
+    ap_model = db.Column(db.String(50), nullable=True)
+    hardware_version = db.Column(db.String(50), nullable=True)
+    software_version = db.Column(db.String(50), nullable=True)
+    description = db.Column(db.String(200), nullable=True)
+
     creation_date = db.Column(db.DateTime, default=datetime.now())
     modification_date = db.Column(
-        db.DateTime, default=datetime.now(), onupdate=datetime.now())
-    
+        db.DateTime, default=datetime.now(), onupdate=datetime.now()
+    )
+
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
