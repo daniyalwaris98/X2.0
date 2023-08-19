4cf0a82111bd944d373d2d40b50a35459542c50e
Muhammad Salman <salmannaseem1103@gmail.com>
2023-08-19 20:08:14 +0500

    Update auto_discovery_dashboard_routes.py

--- backend/atom/app/auto_discovery/auto_discovery_dashboard_routes.py
+++ backend/atom/app/auto_discovery/auto_discovery_dashboard_routes.py
@@ -12,37 +12,39 @@ from app.utilities.db_utils import *
 
 
 def FormatDate(date):
-    #print(date, file=sys.stderr)
+    # print(date, file=sys.stderr)
     if date is not None:
-        result = date.strftime('%d-%m-%Y')
+        result = date.strftime("%d-%m-%Y")
     else:
-        #result = datetime(2000, 1, 1)
+        # result = datetime(2000, 1, 1)
         result = datetime(1, 1, 2000)
 
     return result
+
 
 def FormatStringDate(date):
     print(date, file=sys.stderr)
 
     try:
         if date is not None:
-            if '-' in date:
-                result = datetime.strptime(date,'%d-%m-%Y')
-            elif '/' in date:
-                result = datetime.strptime(date,'%d/%m/%Y')
+            if "-" in date:
+                result = datetime.strptime(date, "%d-%m-%Y")
+            elif "/" in date:
+                result = datetime.strptime(date, "%d/%m/%Y")
             else:
                 print("incorrect date format", file=sys.stderr)
                 result = datetime(2000, 1, 1)
         else:
-            #result = datetime(2000, 1, 1)
+            # result = datetime(2000, 1, 1)
             result = datetime(2000, 1, 1)
     except:
-        result=datetime(2000, 1,1)
+        result = datetime(2000, 1, 1)
         print("date format exception", file=sys.stderr)
 
     return result
 
-@app.route('/getTopOsForDiscovery',methods = ['GET'])
+
+@app.route("/getTopOsForDiscovery", methods=["GET"])
 @token_required
 def GetTopOsForDiscovery(user_data):
     if True:
@@ -52,19 +54,20 @@ def GetTopOsForDiscovery(user_data):
             objList = []
             for row in result:
                 objDict = {}
-                objDict['name']=row[0]
-                objDict['value'] = row[1]
+                objDict["name"] = row[0]
+                objDict["value"] = row[1]
                 objList.append(objDict)
-            return jsonify(objList),200
+            return jsonify(objList), 200
         except Exception as e:
-            print(str(e),file=sys.stderr)
+            print(str(e), file=sys.stderr)
             traceback.print_exc()
-            return str(e),500
+            return str(e), 500
         else:
-            print("Service not Available",file=sys.stderr)
-            return jsonify({"Response":"Service not Available"}),503
-        
-@app.route('/getTopVendorsForDiscovery',methods = ['GET'])
+            print("Service not Available", file=sys.stderr)
+            return jsonify({"Response": "Service not Available"}), 503
+
+
+@app.route("/getTopVendorsForDiscovery", methods=["GET"])
 @token_required
 def GetTopVendorsForDiscovery(user_data):
     if True:
@@ -74,118 +77,102 @@ def GetTopVendorsForDiscovery(user_data):
             objList = []
             for row in result:
                 objDict = {}
-                objDict['name']=row[0]
-                objDict['value'] = row[1]
+                objDict["name"] = row[0]
+                objDict["value"] = row[1]
                 objList.append(objDict)
-            return jsonify(objList),200
+            return jsonify(objList), 200
         except Exception as e:
-            print(str(e),file=sys.stderr)
+            print(str(e), file=sys.stderr)
             traceback.print_exc()
-            return str(e),500
+            return str(e), 500
         else:
-            print("Service not Available",file=sys.stderr)
-            return jsonify({"Response":"Service not Available"}),503
-        
+            print("Service not Available", file=sys.stderr)
+            return jsonify({"Response": "Service not Available"}), 503
 
-@app.route('/getSnmpStatusGraph',methods = ['GET'])
+
+@app.route("/getSnmpStatusGraph", methods=["GET"])
 @token_required
 def GetSnmpStatusGraph(user_data):
-    if True:
-        try:
-            queryString = f"select SNMP_VERSION,count(SNMP_VERSION) from auto_discovery_table group by SNMP_VERSION;"
-            result = db.session.execute(queryString)
-            objList = []
-            for row in result:
-                objDict = {}
-                if (row[0])=='SNMPv2-MIB' or row[0]=='Enabled':
+    try:
+        queryString = f"select snmp_status, count(snmp_status) from auto_discovery_table group by snmp_status;"
+        result = db.session.execute(queryString)
 
-                
-                    objDict['name']='SNMP Enabled'
-                    objDict['value'] = row[1]
-                    objList.append(objDict)
-                else:
-                    objDict['name']='SNMP Disabled'
-                    objDict['value'] = row[1]
-                    objList.append(objDict)
-            # y = {}
-            # for i in objList:
-            #     for j in i:
-            #         y[j] = i[j]
-            print(objList,file=sys.stderr)
-            result = {}
-            for d in objList:
-                if d['name'] in result:
-                    result[d['name']] += d['value']
-                else:
-                    result[d['name']] = d['value']
-                    
-            merged = [{'name': name, 'value': value} for name, value in result.items()]
-            print(merged,file=sys.stderr)
-            return jsonify(merged),200
-        except Exception as e:
-            print(str(e),file=sys.stderr)
-            traceback.print_exc()
-            return str(e),500
-        else:
-            print("Service not Available",file=sys.stderr)
-            return jsonify({"Response":"Service not Available"}),503
-        
+        enable = 0
+        disable = 0
 
-@app.route('/getTopFunctionsForDiscovery',methods = ['GET'])
+        for row in result:
+            if row[0] == "Enabled":
+                enable += 1
+            else:
+                disable += 1
+
+        objList = [
+            {"name": "SNMP Enabled", "value": enable},
+            {"name": "SNMP Disabled", "value": disable},
+        ]
+
+        return jsonify(objList), 200
+
+    except Exception as e:
+        print(str(e), file=sys.stderr)
+        traceback.print_exc()
+        return "Server Error While Fetching Data", 500
+
+
+@app.route("/getTopFunctionsForDiscovery", methods=["GET"])
 @token_required
 def GetTopFunctionsForDiscovery(user_data):
     if True:
         try:
             queryString = f"select `FUNCTION`,count(`FUNCTION`) as count from auto_discovery_table group by `FUNCTION` ORDER by count DESC LIMIT 5;"
             result = db.session.execute(queryString)
-            objList = {'name' : [],
-                       'value' : []}
+            objList = {"name": [], "value": []}
             for row in result:
-                objList['name'].append(row[0].capitalize())
-                objList['value'].append(row[1])
-            return jsonify(objList),200
+                objList["name"].append(row[0].capitalize())
+                objList["value"].append(row[1])
+            return jsonify(objList), 200
         except Exception as e:
-            print(str(e),file=sys.stderr)
+            print(str(e), file=sys.stderr)
             traceback.print_exc()
-            return str(e),500
+            return str(e), 500
         else:
-            print("Service not Available",file=sys.stderr)
-            return jsonify({"Response":"Service not Available"}),503
+            print("Service not Available", file=sys.stderr)
+            return jsonify({"Response": "Service not Available"}), 503
 
 
-@app.route('/getCredentialsGraph',methods = ['GET'])
+@app.route("/getCredentialsGraph", methods=["GET"])
 @token_required
 def GetCredentailsGraph(user_data):
     if True:
         try:
-            objDict = {'name': ["SNMP V1/V2", "SNMP V3", "SSH Login"],
-                       'value':[0,0,0]}
-            
+            objDict = {
+                "name": ["SNMP V1/V2", "SNMP V3", "SSH Login"],
+                "value": [0, 0, 0],
+            }
+
             queryString = f"select SNMP_VERSION,count(SNMP_VERSION) from auto_discovery_table group by SNMP_VERSION;"
             result = db.session.execute(queryString)
-            
+
             for row in result:
-                
-                if (row[0])=='SNMPv2-MIB':
-                    objDict['value'][0]=row[1]
-                elif (row[0])=='SNMPv3-MIB':
-                    objDict['value'][1]=row[1]
+                if (row[0]) == "SNMPv2-MIB":
+                    objDict["value"][0] = row[1]
+                elif (row[0]) == "SNMPv3-MIB":
+                    objDict["value"][1] = row[1]
 
             queryString = f"select SSH_STATUS,count(SSH_STATUS) from auto_discovery_table group by SSH_STATUS;"
             result = db.session.execute(queryString)
 
             for row in result:
-                
-                if (row[0])=='True':
-                    objDict['value'][2]=row[1]
+                if (row[0]) == "True":
+                    objDict["value"][2] = row[1]
 
-            print(objDict,file=sys.stderr)
-            
-            return jsonify(objDict),200
+            print(objDict, file=sys.stderr)
+
+            return jsonify(objDict), 200
         except Exception as e:
-            print(str(e),file=sys.stderr)
+            print(str(e), file=sys.stderr)
             traceback.print_exc()
-            return str(e),500
+            return str(e), 500
         else:
-            print("Service not Available",file=sys.stderr)
-            return jsonify({"Response":"Service not Available"}),503
+            print("Service not Available", file=sys.stderr)
+            return jsonify({"Response": "Service not Available"}), 503
