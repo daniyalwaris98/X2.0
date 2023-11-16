08893b1a21eff6e880f3154bb11f1d259542590d
Hamza Durez <59145858+hamza123232@users.noreply.github.com>
2023-11-16 10:32:30 +0500

    updated password group

--- backend/fast_backend/app/api/v1/atom/routes/password_group_routes.py
+++ backend/fast_backend/app/api/v1/atom/routes/password_group_routes.py
@@ -1,13 +1,13 @@
 from app.api.v1.atom.utils.atom_utils import *
 
 router = APIRouter(
-    prefix="/password-group",
-    tags=["password-group"],
+    prefix="/password_group",
+    tags=["password_group"],
 )
 
 
-@router.post("/add-password-group", responses={
-    200: {"model": str},
+@router.post("/add_password_group", responses={
+    200: {"model": Response200},
     400: {"model": str},
     500: {"model": str}
 })
@@ -23,7 +23,7 @@ def add_password_group(pass_obj: AddPasswordGroupRequestSchema):
         return JSONResponse(content="Error Occurred While Adding Password Group", status_code=500)
 
 
-@router.post("/add-password-groups", responses={
+@router.post("/add_password_groups", responses={
     200: {"model": SummeryResponseSchema},
     500: {"model": str}
 })
@@ -32,30 +32,44 @@ def add_password_groups(pass_list: list[AddPasswordGroupRequestSchema]):
 
         success_list = []
         error_list = []
+        data_lst = []
         for pass_obj in pass_list:
             pass_obj = pass_obj.dict()
 
             msg, status = add_password_group_util(pass_obj, True)
+            # print("message in fields ossssssssssss:::::::::::::::::::::::",msg,file=sys.stderr)
+            # print("status is::::::::::::::::::::::",status,file=sys.stderr)
             if status == 200:
-                success_list.append(msg)
-            else:
-                error_list.append(msg)
+                for key,value in msg.items():
+                                # print("key for msg ares::::::::::::::::::::",key,file=sys.stderr)
+                                # print("values are:::::::::::::::::::::::::::::",value,file=sys.stderr)
+
+                                if key =='data':
+                                    data_lst.append(value)
+                                if key == 'message':
+                                    if value not in success_list:
+                                        # print("values for the message is::::::::::::::::::::::",value,file=sys.stderr)
+                                        success_list.append(value)
+                # success_list.append(msg)
+            # else:
+            #     error_list.append(msg)
 
         response = SummeryResponseSchema(
+            data = data_lst,
             success=len(success_list),
             error=len(error_list),
             success_list=success_list,
             error_list=error_list
         )
 
-        return JSONResponse(content=response, status_code=200)
+        return response
     except Exception:
         traceback.print_exc()
         return JSONResponse(content="Error Occurred While Adding Password Groups", status_code=500)
 
 
-@router.post("/edit-password-group", responses={
-    200: {"model": str},
+@router.post("/edit_password_group", responses={
+    200: {"model": Response200},
     400: {"model": str},
     500: {"model": str}
 })
@@ -71,8 +85,8 @@ def edit_password_group(pass_obj: EditPasswordGroupRequestSchema):
         return JSONResponse(content="Error Occurred While Updating Password Group", status_code=500)
 
 
-@router.post("/delete-password-group", responses={
-    200: {"model": SummeryResponseSchema},
+@router.post("/delete_password_group", responses={
+    200: {"model": DeleteResponseSchema},
     500: {"model": str}
 })
 def delete_password_groups(pass_list: list[DeletePasswordGroupRequestSchema]):
@@ -80,24 +94,35 @@ def delete_password_groups(pass_list: list[DeletePasswordGroupRequestSchema]):
 
         success_list = []
         error_list = []
+        deleted_password_group = []
         for pass_obj in pass_list:
             pass_obj = pass_obj.dict()
-
+            print("password obj is::::::::::::::::::::::::::::",pass_obj,file=sys.stderr)
+            passworg_grp_id = pass_obj['password_group_id']
+            print("password group id for deletion::::::::::::::::::::::::::::::::::::::::::::::::",passworg_grp_id,file=sys.stderr)
+            deleted_passw_group = {}
             password = configs.db.query(PasswordGroupTable).filter(
-                PasswordGroupTable.password_group_id == pass_obj["password_group_id"]).first()
+                    PasswordGroupTable.password_group_id == passworg_grp_id).first()
+            print("password is:::::::::::::::::::::::::::::::::::::",password,file=sys.stderr)
+            if password:
+                deleted_password_group_id = password.password_group_id
+                print("delted password id is:::::::::::::::::::::::::::::::::::::::",deleted_password_group_id,file=sys.stderr)
 
-            if password is None:
-                error_list.append(f"{pass_obj['password_group_id']} : No Password Group Found")
-                continue
-
-            if DeleteDBData(password) == 200:
-                success_list.append(
-                    f"{password.password_group} : Password Group Deleted Successfully")
+                if DeleteDBData(password) == 200:
+                    deleted_passw_group['password_group_id'] = deleted_password_group_id
+                
+                    deleted_password_group.append(deleted_passw_group)
+                    success_list.append(
+                        f"{password.password_group} : Password Group Deleted Successfully")
+                    
+                else:
+                    error_list.append(
+                        f"{password.password_group} : Error While Deleting Password Group")
             else:
-                error_list.append(
-                    f"{password.password_group} : Error While Deleting Password Group")
+                error_list.append(f"{passworg_grp_id} : Password Group ID Not Found")
 
         response = {
+            "data":deleted_password_group,
             "success": len(success_list),
             "error": len(error_list),
             "success_list": success_list,
@@ -111,7 +136,7 @@ def delete_password_groups(pass_list: list[DeletePasswordGroupRequestSchema]):
                             status_code=500)
 
 
-@router.get("/get-password-groups", responses={
+@router.get("/get_password_groups", responses={
     200: {"model": list[GetPasswordGroupResponseSchema]},
     500: {"model": str}
 })
@@ -129,7 +154,7 @@ async def get_password_groups():
                             status_code=500)
 
 
-@router.get("/get-password-group-dropdown", responses={
+@router.get("/get_password_group_dropdown", responses={
     200: {"model": list[str]},
     500: {"model": str}
 })
