3ee450e6a29e20b943bb3513ee689f6b5ef98f0f
muhammad husnain ch <mhusnain1316@gmail.com>
2024-01-09 16:22:49 +0500

    ncm dashboard routing completed

--- frontend/src/containers/ncmModule/index.jsx
+++ frontend/src/containers/ncmModule/index.jsx
@@ -4,12 +4,21 @@ import Card from "../../components/cards";
 import HorizontalMenu from "../../components/horizontalMenu/index";
 import { getPathAllSegments } from "../../utils/helpers";
 import {
+  PAGE_NAME as PAGE_NAME_DASHBOARD,
+  PAGE_PATH as PAGE_PATH_DASHBOARD,
+} from "./dashboard/constants";
+import {
   PAGE_NAME as PAGE_NAME_MANAGE_CONFIGURATIONS,
   PAGE_PATH as PAGE_PATH_MANAGE_CONFIGURATIONS,
 } from "./manageConfigurations/constants";
 export const MODULE_PATH = "ncm_module";
 
 const menuItems = [
+  {
+    id: PAGE_PATH_DASHBOARD,
+    name: PAGE_NAME_DASHBOARD,
+    path: PAGE_PATH_DASHBOARD,
+  },
   {
     id: PAGE_PATH_MANAGE_CONFIGURATIONS,
     name: PAGE_NAME_MANAGE_CONFIGURATIONS,
--- frontend/src/routes/ncmModuleRoutes.js
+++ frontend/src/routes/ncmModuleRoutes.js
@@ -1,15 +1,18 @@
 import React from "react";
 import NCMModule from "../containers/ncmModule";
+import Dashboard from "../containers/ncmModule/dashboard"
 import ConfigData from "../containers/ncmModule/manageConfigurations";
 import ManageConfigurationsLanding from "../containers/ncmModule/manageConfigurationsLanding";
 import ConfigurationBackups from "../containers/ncmModule/manageConfigurationsLanding/configurationBackups";
 import RemoteCommandSender from "../containers/ncmModule/manageConfigurationsLanding/remoteCommandSender";
 import { Navigate } from "react-router-dom";
+import { PAGE_PATH as PAGE_PATH_DASHBOARD } from "../containers/ncmModule/dashboard/constants";
 import { PAGE_PATH as PAGE_PATH_CONFIG_DATA } from "../containers/ncmModule/manageConfigurations/constants";
 import { LANDING_PAGE_PATH as LANDING_PAGE_PATH_MANAGE_CONFIGURATIONS } from "../containers/ncmModule/manageConfigurationsLanding";
 import { PAGE_PATH as PAGE_PATH_CONFIGURATION_BACKUPS } from "../containers/ncmModule/manageConfigurationsLanding/configurationBackups/constants";
 import { PAGE_PATH as PAGE_PATH_REMOTE_COMMAND_SENDER } from "../containers/ncmModule/manageConfigurationsLanding/remoteCommandSender/constants";
 import { MODULE_PATH } from "../containers/ncmModule";
+
 
 const routes = {
   path: MODULE_PATH,
@@ -17,7 +20,11 @@ const routes = {
   children: [
     {
       path: `/${MODULE_PATH}`,
-      element: <Navigate to={PAGE_PATH_CONFIG_DATA} replace />,
+      element: <Navigate to={PAGE_PATH_DASHBOARD} replace />,
+    },
+    {
+      path: PAGE_PATH_DASHBOARD,
+      element: <Dashboard />,
     },
     {
       path: PAGE_PATH_CONFIG_DATA,
