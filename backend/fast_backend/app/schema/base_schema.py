548c5823706969a44e225cc160ba93a2458fa25e
Hamza Durez <59145858+hamza123232@users.noreply.github.com>
2024-01-29 18:35:49 +0500

    Discovery,IPAM,Monitoring,Huawei support added for the NCM

--- backend/fast_backend/app/schema/base_schema.py
+++ backend/fast_backend/app/schema/base_schema.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel,Field,constr,validator
-
+from typing import List, Optional, Union
 
 class BaseSchema(BaseModel):
     def __getitem__(self, key):
@@ -48,3 +48,35 @@ class NameValueDictResponseSchema(BaseSchema):
 
 class IpAddressRequestSchema(BaseSchema):
     ip_address: str
+
+
+class ModelBaseInfo(BaseModel):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+
+
+class FindBase(BaseModel):
+    ordering: Optional[str]
+    page: Optional[int]
+    page_size: Optional[Union[int, str]]
+
+
+class SearchOptions(FindBase):
+    total_count: Optional[int]
+
+
+class FindResult(BaseModel):
+    founds: Optional[List]
+    search_options: Optional[SearchOptions]
+
+
+class FindDateRange(BaseModel):
+    created_at__lt: str
+    created_at__lte: str
+    created_at__gt: str
+    created_at__gte: str
+
+
+class Blank(BaseModel):
+    pass
