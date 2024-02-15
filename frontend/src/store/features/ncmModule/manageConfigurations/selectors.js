c534fd9245b697fd6220ea75001cfba77139e2dd
muhammad husnain ch <mhusnain1316@gmail.com>
2024-02-15 16:12:01 +0500

    ncm internal graph completed

--- frontend/src/store/features/ncmModule/manageConfigurations/selectors.js
+++ frontend/src/store/features/ncmModule/manageConfigurations/selectors.js
@@ -4,3 +4,7 @@ export const selectAtomsToAddInNcmDevicesData = (state) =>
   state.ncm_manage_configurations.atoms_to_add_in_ncm_devices;
 export const selectSelectedDevice = (state) =>
   state.ncm_manage_configurations.selected_device;
+  export const selectSeverity = (state) =>
+  state.ncm_manage_configurations.severity_data;
+  export const selectDeviceType = (state) =>
+  state.ncm_manage_configurations.device_type_data;
