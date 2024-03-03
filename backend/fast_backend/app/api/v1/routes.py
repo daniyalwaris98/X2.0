fc9eb8b962581280a649c502815639172b778e89
Hamza Durez <59145858+hamza123232@users.noreply.github.com>
2024-03-03 14:23:25 +0500

    routes updated

--- backend/fast_backend/app/api/v1/routes.py
+++ backend/fast_backend/app/api/v1/routes.py
@@ -12,7 +12,7 @@ from app.api.v1.ipam.ipam_base_routes import routers as ipam_router
 from app.api.v1.users.user_base_router import routers as user_router
 from app.api.v1.main.routes.main_dashboard_routes import router as main_dashboard_router
 from app.api.v1.cloud_monitoring.cloud_monitoring_base_router import routers as cloud_monitoring_router
-from app.api.v1.fire_base.fire_base_router import routers as firebase_router
+# from app.api.v1.fire_base.fire_base_router import routers as firebase_router
 from app.api.v1.common_routes.common_base_router import routers as common_base_router
 
 
@@ -30,7 +30,7 @@ router_list = [
     user_router,
     main_dashboard_router,
     cloud_monitoring_router,
-    firebase_router,
+    # firebase_router,
     common_base_router
 ]
 
