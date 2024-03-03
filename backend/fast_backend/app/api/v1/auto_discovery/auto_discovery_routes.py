c41a46df79ac31197ba76f1cb320e1ee14a48207
Hamza Durez <59145858+hamza123232@users.noreply.github.com>
2024-03-03 16:56:03 +0500

    auto discovery updated

--- backend/fast_backend/app/api/v1/auto_discovery/auto_discovery_routes.py
+++ backend/fast_backend/app/api/v1/auto_discovery/auto_discovery_routes.py
@@ -254,8 +254,8 @@ async def auto_discovery_background_task(subnet):
         if network is not None:
             start_ip = calculate_start_ip(subnet.subnet)
             end_ip = calculate_end_ip(subnet.subnet)
-            results = get_range_inventory_data(start_ip, end_ip)
-            results = await asyncio.to_thread(get_range_inventory_data, start_ip, end_ip)
+            # results = get_range_inventory_data(start_ip, end_ip)
+            results = await get_range_inventory_data(start_ip, end_ip)
             actual_host = len(results)
             # results = auto_discover.get_range_inventory_data(
             #     network.subnet, network.excluded_ip_range
