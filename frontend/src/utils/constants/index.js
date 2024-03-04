812f0a0f12f3cd4649694d9d2d5bc9e39f90a8b2
alihassan2000 <102433608+AliHassan2000@users.noreply.github.com>
2024-03-04 18:00:14 +0500

    Update index.js

--- frontend/src/utils/constants/index.js
+++ frontend/src/utils/constants/index.js
@@ -1,4 +1,4 @@
-export const API_ENDPOINT_URL = "http://192.168.10.150:8080";
+export const API_ENDPOINT_URL = "http://13.41.164.246:8080";
 // export const API_ENDPOINT_URL = "http://localhost:8080";
 
 
