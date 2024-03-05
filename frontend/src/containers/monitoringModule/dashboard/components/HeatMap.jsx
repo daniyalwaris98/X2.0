21cdbb8c211a678def098967f109e4742e0b805d
muhammad husnain ch <mhusnain1316@gmail.com>
2024-03-05 12:58:18 +0500

    changes fixe

--- frontend/src/containers/monitoringModule/dashboard/components/HeatMap.jsx
+++ frontend/src/containers/monitoringModule/dashboard/components/HeatMap.jsx
@@ -10,7 +10,7 @@ function HeatMap({ data }) {
       rowsArray.push(
         <div key={i} style={{ display: "flex", justifyContent: "start", margin: "20px 0 20px 50px" }}>
           {rowData.map((item, index) => (
-            <div key={index} style={{ width: "63px", height: "60px", backgroundColor: item.fill, borderRadius: "2px", marginRight: "20px", borderRadius:"7px" }}></div>
+            <div key={index} style={{ width: "63px", height: "60px", backgroundColor: item.fill, borderRadius: "7px", marginRight: "20px" }}></div>
           ))}
         </div>
       );
@@ -21,6 +21,24 @@ function HeatMap({ data }) {
   return (
     <div>
       {createRows()}
+      <div style={{ display: "flex", justifyContent: "start", margin: "20px 0 20px 50px" }}>
+        <div style={{ width: "63px", height: "60px", backgroundColor: "green", borderRadius: "7px", marginRight: "20px" }}></div>
+        <div style={{ width: "63px", height: "60px", backgroundColor: "red", borderRadius: "7px", marginRight: "20px" }}></div>
+        <div style={{ width: "63px", height: "60px", backgroundColor: "yellow", borderRadius: "7px", marginRight: "20px" }}></div>
+      </div>
+      <div style={{ display: "start", justifyContent: "center",gap:"20px", margin: "0 0 20px 120px" }}>
+        <label style={{  borderRadius: "50%", width: "10px", height: "10px", marginRight: "20px",color:"#434343",fontWeight:"500" }}>Clear</label>
+        <label style={{  borderRadius: "50%", width: "10px", height: "10px", marginRight: "20px",color:"#434343",fontWeight:"500" }}>Device Down</label>
+        <label style={{ borderRadius: "50%", width: "10px", height: "10px", marginRight: "20px" ,color:"#434343",fontWeight:"500"}}>Attention</label>
+        <label style={{ borderRadius: "50%", width: "10px", height: "10px", marginRight: "20px" ,color:"#434343",fontWeight:"500"}}>Critical</label>
+        <label style={{  borderRadius: "50%", width: "10px", height: "10px", marginRight: "20px",color:"#434343",fontWeight:"500" }}>Not Monitored</label>
+
+
+
+
+
+        
+      </div>
     </div>
   );
 }
