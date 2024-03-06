f10cb83c16cacd6b6b20564f7a847a0206e434c5
muhammad husnain ch <mhusnain1316@gmail.com>
2024-03-06 13:20:17 +0500

    graph fixed

--- frontend/src/containers/autoDiscoveryModule/dashboard/components/TopVendorForDiscovery.jsx
+++ frontend/src/containers/autoDiscoveryModule/dashboard/components/TopVendorForDiscovery.jsx
@@ -21,6 +21,69 @@ const TopVendorForDiscovery = ({ data }) => {
       "#9E00D5",
     ];
 
+    // const option = {
+    //   tooltip: {
+    //     show: true,
+    //     trigger: "item",
+    //   },
+    //   angleAxis: {
+    //     type: "category",
+    //     startAngle: 270,
+    //     axisLine: {
+    //       show: false,
+    //     },
+    //     axisLabel: {
+    //       show: false,
+    //     },
+    //     axisTick: {
+    //       show: false,
+    //     },
+    //   },
+    //   radiusAxis: {
+    //     max: 12,
+    //     startAngle: 90,
+    //     axisLine: {
+    //       show: false,
+    //     },
+    //     axisLabel: {
+    //       show: false,
+    //     },
+    //     axisTick: {
+    //       show: false,
+    //     },
+    //   },
+    //   polar: {
+    //     radius: [7, "75%"], // Adjust the inner radius to decrease gap from the top
+    //     center: ["50%", "45%","0%", "0%"], // Adjusted center to be in the middle horizontally and at the top vertically
+
+    //   },
+    //   series: data.map((item, index) => ({
+    //     type: "bar",
+    //     data: [item.value],
+    //     coordinateSystem: "polar",
+    //     name: item.name,
+    //     color: colors[index % colors.length], // Use colors from the array
+    //     label: {
+    //       show: true,
+    //       position: "outside", // Labels at the center of the bar
+    //       formatter: "{c}", // Display data value as label
+    //     },
+    //   })),
+    //   legend: {
+    //     show: true,
+    //     orient: "horizontal", // Display legend horizontally
+    //     y: "bottom", // Adjust legend position
+    //     icon: "circle",
+    //     itemGap: 10, // Adjust the gap between legend items
+    //     itemWidth: 20, // Adjust the width of legend items
+    //     itemHeight: 10, // Adjust the height of legend items
+    //   },
+    //   emphasis: {
+    //     focus: "series",
+    //   },
+    //   barGap: "5%",
+    // };
+    
     const option = {
       tooltip: {
         show: true,
@@ -53,9 +116,7 @@ const TopVendorForDiscovery = ({ data }) => {
         },
       },
       polar: {
-        radius: [7, "75%"], // Adjust the inner radius to decrease gap from the top
-        center: ["50%", "45%","0%", "0%"], // Adjusted center to be in the middle horizontally and at the top vertically
-
+        radius: [0, "70%"], // Adjust the inner radius to decrease gap from the top
       },
       series: data.map((item, index) => ({
         type: "bar",
@@ -64,9 +125,12 @@ const TopVendorForDiscovery = ({ data }) => {
         name: item.name,
         color: colors[index % colors.length], // Use colors from the array
         label: {
-          show: true,
-          position: "inside", // Labels at the center of the bar
-          formatter: "{c}", // Display data value as label
+          show: false,
+          formatter: function (params) {
+            // Display label only on hover
+            return params.dataIndex === index ? params.value : "";
+          },
+          position: "outside", // Labels at the center of the bar
         },
       })),
       legend: {
@@ -84,7 +148,6 @@ const TopVendorForDiscovery = ({ data }) => {
       barGap: "5%",
     };
     
-
     option && myChart.setOption(option);
 
     return () => {
