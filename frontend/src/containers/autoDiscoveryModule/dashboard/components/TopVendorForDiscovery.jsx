a6d10e3b578ceaf91145698664812baf35e20ace
muhammad husnain ch <mhusnain1316@gmail.com>
2024-03-06 12:29:46 +0500

    Top vendor for discovery fixed on main dashboard

--- frontend/src/containers/autoDiscoveryModule/dashboard/components/TopVendorForDiscovery.jsx
+++ frontend/src/containers/autoDiscoveryModule/dashboard/components/TopVendorForDiscovery.jsx
@@ -12,7 +12,14 @@ const TopVendorForDiscovery = ({ data }) => {
     const myChart = echarts.init(chartDom);
 
     // Define colors array
-    const colors = ["#63ABFD", "#84CC7D", "#3D9E47", "#5F83CA", "#E69B43", "#9E00D5"];
+    const colors = [
+      "#63ABFD",
+      "#84CC7D",
+      "#3D9E47",
+      "#5F83CA",
+      "#E69B43",
+      "#9E00D5",
+    ];
 
     const option = {
       tooltip: {
@@ -48,29 +55,35 @@ const TopVendorForDiscovery = ({ data }) => {
       polar: {
         radius: [2, "70%"],
       },
-      series: data.map((item, index) => ({
-        type: "bar",
-        data: [item.value],
-        coordinateSystem: "polar",
-        name: item.name,
-        color: colors[index % colors.length], // Use colors from the array
-        label: {
-          show: true,
-          position: "inside", // Adjust label position as needed
-          formatter: "{c}", // Display data value as label
-        },
-      })),
+
+      
+        series: data.map((item, index) => ({
+          type: "bar",
+          data: [item.value],
+          coordinateSystem: "polar",
+          name: item.name,
+          color: colors[index % colors.length], // Use colors from the array
+          label: {
+            show: true,
+            position: "inside", // Labels at the center of the bar
+            formatter: "{c}", // Display data value as label
+          },
+        })),
+       
       legend: {
         show: true,
-        y: "bottom",
+        orient: "horizontal", // Display legend horizontally
+        y: "bottom", // Adjust legend position
         icon: "circle",
-        
+        itemGap: 10, // Adjust the gap between legend items
+        itemWidth: 20, // Adjust the width of legend items
+        itemHeight: 10, // Adjust the height of legend items
       },
       emphasis: {
         focus: "series",
       },
+
       barGap: "13%",
-     
     };
 
     option && myChart.setOption(option);
@@ -80,7 +93,7 @@ const TopVendorForDiscovery = ({ data }) => {
     };
   }, [data]);
 
-  return <div id="main" style={{ width: "100%", height: "350px" }}></div>;
+  return <div id="main" style={{ width: "100%", height: "380px" }}></div>;
 };
 
 export default TopVendorForDiscovery;
